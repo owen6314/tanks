3acfc32cb9f56a2cf42f1883f2f2e9cdf79f53cf
owen <thss15_zhangqr@163.com>
2017-09-11 17:55:36 +0800

    init

---
+++ Assets/Scripts/UIDirectionControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDirectionControl : MonoBehaviour {
+	public bool m_UseRelativeRotation = true;
+
+	private Quaternion m_RelativeRotation;
+	// Use this for initialization
+	public void Start () 
+	{
+		m_RelativeRotation = transform.parent.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (m_UseRelativeRotation)
+			transform.rotation = m_RelativeRotation;	
+	}
+}
